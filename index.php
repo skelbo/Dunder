930197d35c2cd412a9b49754387943847dbcb095
EthicalBoris <38215510+EthicalBoris@users.noreply.github.com>
2018-08-22 02:43:54 +0100

    css linking and caterogies test

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
  <head>
   <meta charset="UTF-8" />
   </title>Site/Tab Title</title>
-  <style>
-   body {color: #000000; background: white;}
-   h1 {color: navy; font-family: Arial;}
-  </style>
+  <link rel="stylesheet" href="DzikStyle.css" type="text/css" />  
 </head>
 <body>
+  <div class="bg">
+  <ul>
+     <li><a href="Index.php">Home</a></li>
+	 <li><a href="Books.php">Books</a></li>
+	 <li><a href="Games.php">Games</a></li>
+  </ul>
+  </div>
  <h1>Dunder Mifflin</h1>
  <h2>Online shopping</h2>
  <p>Dunder Mifflin badsjdaskdha description</p><br />
